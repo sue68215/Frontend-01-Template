680ad01a5b52059bfea068bdee4bd6f189c877d3
sue <wangl585@chinaunicom.cn>
2020-06-03 21:31:43 +0800

    Update

---
+++ week08/match.js
@@ -0,0 +1,21 @@
+function match(element, selector) {
+  if (!selector || !element.attributes) {
+    return false
+  }
+  if (selector.charAt(0) == '#') {
+    let attr = element.attributes.filter((attr) => attr.name == 'id')[0]
+    if (attr && attr.value == selector.replace('#', '')) {
+      return true
+    }
+  } else if (selector.charAt(0) == '.') {
+    let attr = element.attributes.filter((attr) => attr.name == 'class')[0]
+    if (attr && attr.value == selector.replace('.', '')) {
+      return true
+    }
+  } else {
+    if (element.tagName == selector) {
+      return true
+    }
+  }
+  return false
+}
